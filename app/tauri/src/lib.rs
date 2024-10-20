d47d095b53569a18bfcafa17e4e26981fda82351
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-10-20 19:52:42 +0100

    chore: add application plugins
    sql - for data storage
    stronghold for auth credentials
    singleinstance to prevent race error
    notification for native notification
    clipboard

--- app/tauri/src/lib.rs
+++ app/tauri/src/lib.rs
@@ -1,6 +1,13 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
+        // .plugin(tauri_plugin_updater::Builder::new().build())
+        .plugin(tauri_plugin_sql::Builder::new().build())
+        // .plugin(tauri_plugin_single_instance::init())
+        // .plugin(tauri_plugin_stronghold::Builder::new(|pass| todo!()).build())
+        .plugin(tauri_plugin_notification::init())
+        .plugin(tauri_plugin_clipboard_manager::init())
+        // .plugin(tauri_plugin_autostart::init())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
