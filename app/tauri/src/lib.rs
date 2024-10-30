0cd81385a93459641352f87dc03f6c115e84c763
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-10-31 00:23:54 +0100

    feat(grpc_destop): add client stub to tauri layer

--- app/tauri/src/lib.rs
+++ app/tauri/src/lib.rs
@@ -1,11 +1,11 @@
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub mod api;
 pub mod app_state;
-pub mod commands;
+// pub mod commands;
 pub mod config;
 pub mod jwt;
 
-use commands::authentication;
+// use commands::authentication;
 
 pub fn run() {
     tauri::Builder::default()
@@ -15,7 +15,7 @@ pub fn run() {
         // })
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .invoke_handler(tauri::generate_handler![authentication::sign_up])
+        // .invoke_handler(tauri::generate_handler![authentication::sign_up])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
