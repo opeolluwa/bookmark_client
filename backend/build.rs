91953d4980cf168f3735faeb2e74c140c3d8bcca
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-01 04:36:00 +0100

    refactor: move service to be fully grpc

--- backend/build.rs
+++ backend/build.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .protoc_arg("--experimental_allow_proto3_optional")
         .protoc_arg("--proto_path=../proto")
-        // .build_client(true) // don't compile the client code
+        .build_client(false) // don't compile the client code
         .build_server(true)
         .file_descriptor_set_path(out_dir.join("store_descriptor.bin"))
         .out_dir("./src/proto")
