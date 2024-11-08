7a8b8a857220758fbc407d506ab5c85abfb28870
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-08 02:15:06 +0100

    update mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -123,5 +123,71 @@ function DesktopAppEntry() {
 }
 
 function MobileAppEntry() {
-  return <div>MobileAppEntry</div>;
+  const [form] = Form.useForm();
+  const submitForm: FormProps<FormFieldTypes>["onFinish"] = (values) => {
+    const new_user: LoginData = {
+      email: values.email?.trim(),
+      password: values.password?.trim(),
+    };
+    invoke("sign_in", { user: new_user }).then((res) => {
+      console.log({ res });
+    });
+  };
+  return (
+    <View className=" pt-4 px-6">
+      <Form
+        initialValues={{ remember: true }}
+        onFinish={submitForm}
+        autoComplete="off"
+        name="save-data"
+        layout="vertical"
+        className=" flex flex-col rounded-lg shadow-gray-300 bg-white py-6 "
+        form={form}
+      >
+        <View className="mb-12">
+          <Heading className="font-semibold">Welcome back!</Heading>
+          <Text className="leading-1">Sign in to continue</Text>
+        </View>
+        <Form.Item<FormFieldTypes>
+          label="Email"
+          name="email"
+          rules={[{ required: true, message: "email is required!" }]}
+        >
+          <Input
+            autoFocus
+            type="email"
+            name="email"
+            className="w-full rounded-lg py-3 focus:border-app-500 focus:outline-none border bg-white border-gray-300 block placeholder:pb-2 px-2"
+          />
+        </Form.Item>
+        <Form.Item<FormFieldTypes>
+          label="Password"
+          name="password"
+          rules={[{ required: true, message: "password is required!" }]}
+        >
+          <Input.Password
+            type="password"
+            name="password"
+            className="w-full rounded-lg py-3 focus:border-app-500 focus:outline-none border bg-white border-gray-300 placeholder:pb-2 px-2 "
+            iconRender={(visible) =>
+              visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />
+            }
+          />
+        </Form.Item>
+
+        <Button
+          href="/dashboard"
+          className=" bg-app-600 text-center w-full py-2 text-white"
+        >
+          Sign in
+        </Button>
+        <SmallText
+          href="/authentication/password-reset"
+          className="text-gray-500 font-medium no-underline text-sm mt-4"
+        >
+          Forgotten password?
+        </SmallText>
+      </Form>
+    </View>
+  );
 }
