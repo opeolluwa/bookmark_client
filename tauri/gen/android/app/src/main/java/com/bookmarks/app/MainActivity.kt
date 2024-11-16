5524b6862c93f05a6b778856569235084adef362
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-16 13:46:50 +0100

    feat(custom text action select): configure intent

--- tauri/gen/android/app/src/main/java/com/bookmarks/app/MainActivity.kt
+++ tauri/gen/android/app/src/main/java/com/bookmarks/app/MainActivity.kt
@@ -1,17 +1,15 @@
 package com.bookmarks.app
 
-import android.os.Bundle
 import android.content.Intent
+import android.os.Bundle
 
-class MainActivity : TauriActivity() 
+class MainActivity : TauriActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.process_text_main);
+        val text: CharSequence? = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
+        // Process the text as string to remove color formatting or styles
+        val formattedText = text.toString()
+    }
+}
 
-
-    // override fun onCreate(savedInstanceState: Bundle?) {
-    //     super.onCreate(savedInstanceState)
-    //     setContentView(R.layout.process_text_main)
-
-    //     val text: CharSequence? = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
-    //     // Process the text as string to remove color formatting or styles 
-    //     val formattedText = text.toString()
-    // }
-// }
