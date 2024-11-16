4d243d342ac10ac3667884456f8bb728c52336ff
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-16 14:07:37 +0100

    fixes(user text select action):

--- tauri/gen/android/app/src/main/java/com/bookmarks/app/MainActivity.kt
+++ tauri/gen/android/app/src/main/java/com/bookmarks/app/MainActivity.kt
@@ -6,7 +6,7 @@ import android.os.Bundle
 class MainActivity : TauriActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.process_text_main);
+        // setContentView(R.layout.process_text_main);
         val text: CharSequence? = intent?.getCharSequenceExtra(Intent.EXTRA_PROCESS_TEXT)
         // Process the text as string to remove color formatting or styles
         val formattedText = text.toString()
