b9fc669f465986909f460832f4402c8ef9cc6d34
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-11-19 22:18:25 +0100

    refactor

--- tauri/src/lib.rs
+++ tauri/src/lib.rs
@@ -1,11 +1,7 @@
-// use lazy_static::lazy_static;
 use tauri_plugin_store::StoreExt;
-lazy_static::lazy_static! {
-    pub static ref GRPC_SERVER_ENDPOINT: String = String::from("http://127.0.0.1:50051");
-}
+
 
 pub mod app_state;
-pub mod commands;
 pub mod config;
 pub mod helpers;
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -21,17 +17,12 @@ pub fn run() {
             // app.manage(app_state);
             Ok(())
         })
-         .plugin(tauri_plugin_sharesheet::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::new().build())
         .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_clipboard_manager::init())
-        .invoke_handler(tauri::generate_handler![
-            commands::authentication::sign_in,
-            commands::bookmarks::get_all_bookmark_collections,
-            commands::bookmarks::create_bookmark_collection
-        ])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
