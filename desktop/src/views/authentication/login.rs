a8c7308d0671ee7541cbf471385e1d25d1e52d88
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-12-17 00:59:57 +0100

    feat: imple the login page

--- desktop/src/views/authentication/login.rs
+++ desktop/src/views/authentication/login.rs
@@ -18,18 +18,18 @@ pub fn LoginPage() -> impl leptos::IntoView {
     view! {
         <div class="relative">
             <div class="m-6 text-center">
-                <Heading class="font-bold".into()>Welcome back!</Heading>
+                <Heading class="font-bold dark:text-gray-500">Welcome back!</Heading>
                 <SmallText class="leading-1 mt-2 text-sm small-text">Sign in to continue</SmallText>
             </div>
 
             <form class="flex flex-col gap-y-4 mt-6">
 
-                <div class="form-input">
-                    <label for="email" class="text-black">Email</label>
+                <div class="form-input desktop">
+                    <label for="email" >Email</label>
                     <input type="email" placeholder="type your email" />
                 </div>
-                <div class="form-input">
-                    <label for="password" class="text-black" >Password</label>
+                <div class="form-input desktop">
+                    <label for="password" >Password</label>
                     <input type="password"   placeholder="type your password" />
                 </div>
 
@@ -37,7 +37,7 @@ pub fn LoginPage() -> impl leptos::IntoView {
                     href="/dashboard"
                     // disabled=is_loading
                     type="submit"
-                    class="btn w-full rounded-lg py-4 bg-gray-400 text-white"
+                    class="btn w-full rounded-lg py-4 bg-app-600 text-white font-medium"
                 >
                     {if is_loading.get() == true {
                         Either::Right(
@@ -50,28 +50,10 @@ pub fn LoginPage() -> impl leptos::IntoView {
                 </a>
             // href="/mobile/authentication/forgotten-password"
             </form>
-            <a href="/forgotten-password" class="text-app block  text-sm font-bold mt-3">
+            <a href="/forgotten-password" class="text-gray-400 block text-center text-sm  mt-6 dark:text-gray-500 flex justify-end">
                 Forgotten password?
             </a>
-            <div class="flex items-center hidden justify-center ">
-                <button class="btn  shadow-none bg-app-50 border-none ">
-                    <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke-width="1.5"
-                        stroke="currentColor"
-                        class="size-6 text-app bg-app-50/50"
-                    >
-                        <path
-                            stroke-linecap="round"
-                            stroke-linejoin="round"
-                            d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
-                        />
-                    </svg>
-
-                </button>
-            </div>
+           
         </div>
     }
 }
