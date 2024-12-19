1e861b1808360a529921e91021b6e8df85d02ad0
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-12-19 12:47:59 +0100

    refactor

--- components/src/forms/mod.rs
+++ components/src/forms/mod.rs
@@ -1,8 +1,2 @@
 pub mod sign_up;
 
-pub trait SubmitForm {
-    fn submit_form<T, E>(
-        &self,
-        endpoint: &'static str,
-    ) -> impl std::future::Future<Output = Result<T, E>> + Send;
-}
