181b0e85e1ef92db59e23a3eda6ccdb1a308984a
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2024-12-19 12:47:27 +0100

    fix: impl calss binding

--- components/src/layout/view.rs
+++ components/src/layout/view.rs
@@ -3,5 +3,5 @@ use leptos::{prelude::Children, view};
 
 #[leptos::component]
 pub fn View(children: Children, #[prop(optional)] class: &'static str) -> impl leptos::IntoView {
-    view! { <div class="class">{children()}</div> }
+    view! { <div class=format!("{class}")>{children()}</div> }
 }
