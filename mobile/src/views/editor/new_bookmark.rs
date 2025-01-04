8878260fd8ed0f4381e024e71263fb0a4eb1f0d7
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-01-04 18:48:20 +0100

    feat: add safe icon

--- mobile/src/views/editor/new_bookmark.rs
+++ mobile/src/views/editor/new_bookmark.rs
@@ -1,3 +1,4 @@
+use bookmark_components::icon::HeroIcon;
 use leptos::prelude::ClassAttribute;
 use leptos::prelude::ElementChild;
 use leptos::view;
@@ -6,16 +7,29 @@ use bookmark_components::editor::mobile_editor::MarkdownEditor;
 use bookmark_components::icons::chevron::ChevronLeftIcon;
 use bookmark_components::typography::heading::PageHeading;
 
+use leptos_heroicons::size_24::solid::FolderArrowDown as SafeIcon;
+
 #[leptos::component]
 pub fn NewBookmarkPage() -> impl leptos::IntoView {
+    let save_icon = view! {<SafeIcon/>};
+
     view! {
-        <header class="flex bg-white fixed top-0 py-4  left-0 mb-5 right-0 w-full border-b gap-2 px-4 items-center  dark:bg-gray-900/70 dark:border-none z-[1000]">
+        <header class="flex bg-white fixed top-0 py-4  left-0 mb-5 right-0 w-full border-b gap-2 px-4 items-center  dark:bg-gray-900/70 dark:border-none z-[1000] relative">
             <a href="/dashboard">
                 <ChevronLeftIcon />
             </a>
             <PageHeading text="New Bookmark" />
         </header>
+<div class="relative">
 
         <MarkdownEditor />
+
+
+         <button
+            class=" size-10 p-2 border-transparent text-white dark:text-gray-400 bg-app shadow fixed  rounded-full bottom-20 right-4"
+        >
+            <HeroIcon icon_data=save_icon />
+        </button>
+</div>
     }
 }
