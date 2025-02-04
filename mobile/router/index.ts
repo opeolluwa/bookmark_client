242bce01b69f29a8eb59b6ee239c3a49ada54115
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-02-04 19:29:34 +0100

    use vue

--- mobile/router/index.ts
+++ mobile/router/index.ts
@@ -1,12 +1,14 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import HomeScreen from '@mobile/views/dashboard/HomeScreen.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-{
-  
-}
+    {
+      path: '/',
+      component: HomeScreen,
+    },
   ],
 })
 
---
+++ mobile/views/dashboard/HomeScreen.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    home screen
+  </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style>
+
+</style>
