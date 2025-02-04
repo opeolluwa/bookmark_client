4fe210fdb4c44c63005b041799f3055f68fe7fc3
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-02-04 17:51:02 +0100

    update

--- bookmarks/src/main.ts
+++ bookmarks/src/main.ts
@@ -1,17 +1,32 @@
+import '@/styles/app.css'
+import Aura from '@primevue/themes/aura'
+import PrimeVue from 'primevue/config'
+
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
-import Mobile from '../apps/mobile/App.vue'
-import Desktop from '../apps/desktop/App.vue'
+import Mobile from '@mobile/App.vue'
+import Desktop from '@desktop/App.vue'
 
 const isMobile = true
 
 const app = createApp(isMobile ? Mobile : Desktop)
 
-import mobileRouter from '../apps/mobile/router'
-import desktopRouter from '../apps/desktop/router'
+import mobileRouter from '../mobile/router'
+import desktopRouter from '../desktop/router'
 
 app.use(createPinia())
 app.use(isMobile ? mobileRouter : desktopRouter)
+// app.use(PrimeVue, {
+//   theme: {
+//     preset: Aura,
+//     options: {
+//       cssLayer: {
+//         // name: 'primevue',
+//         // order: 'app-styles, primevue, another-css-library',
+//       },
+//     },
+//   },
+// })
 
 app.mount('#app')
--- bookmarks/vite.config.ts
+++ bookmarks/vite.config.ts
@@ -3,16 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@mobile': fileURLToPath(new URL('./mobile', import.meta.url)),
+      '@desktop': fileURLToPath(new URL('./desktop', import.meta.url)),
+      '@shared': fileURLToPath(new URL('./shared', import.meta.url)),
     },
   },
 })
