9fcd86e823b063e4f102a812cd646316cf1760bd
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-02-04 16:28:07 +0100

    update

--- components/src/forms/request.rs
+++ components/src/forms/request.rs
@@ -6,19 +6,19 @@ pub static DEVELOPMENT_BASE_DIR: &'static str = "http://localhost:4576";
 
 impl RequestEndpoint {
     pub fn new(path: &str) -> String {
-        // let base_url = match std::env::var("BASE_URL") {
-        //     Ok(base_url) => base_url,
-        //     Err(_) => {
-        //         if cfg!(target_os = "android") {
-        //             EMULATOR_BASE_DIR.to_string()
-        //         } else {
-        //             DEVELOPMENT_BASE_DIR.to_string()
-        //         }
-        //     }
-        // };
+        let base_url = match std::env::var("BASE_URL") {
+            Ok(base_url) => base_url,
+            Err(_) => {
+                if cfg!(target_os = "android") {
+                    EMULATOR_BASE_DIR.to_string()
+                } else {
+                    DEVELOPMENT_BASE_DIR.to_string()
+                }
+            }
+        };
         format!(
             "{base_url}/v1/{path}",
-            base_url = EMULATOR_BASE_DIR,
+            // base_url = ,
             path = path
         )
     }
